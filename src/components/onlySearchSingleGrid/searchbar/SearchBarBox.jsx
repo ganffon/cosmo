2cdece7062f1d716a781afb1c69e101ed055cd37
kimdh <ganffon57@naver.com>
2023-03-18 15:42:26 +0900

    Input Text Edit

--- src/components/onlySearchSingleGrid/searchbar/SearchBarBox.jsx
+++ src/components/onlySearchSingleGrid/searchbar/SearchBarBox.jsx
@@ -16,6 +16,7 @@ import { OnlySearchSingleGridEvent } from "components/onlySearchSingleGrid/OnlyS
 import restAPI from "api/restAPI";
 import NoticeSnack from "components/alert/NoticeSnack";
 import * as S from "./SearchBarBox.styled";
+import InputAdornment from "@mui/material/InputAdornment";
 
 export const SearchBarBoxEvent = createContext();
 
@@ -190,6 +191,16 @@ function SearchBarBox(props) {
     }
   };
 
+  const top100Films = [
+    { label: "The Shawshank Redemption", year: 1994 },
+    { label: "The Godfather", year: 1972 },
+    { label: "The Godfather: Part II", year: 1974 },
+    { label: "The Dark Knight", year: 2008 },
+    { label: "12 Angry Men", year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: "Pulp Fiction", year: 1994 },
+  ];
+
   return (
     <SearchBarBoxEvent.Provider
       value={{
@@ -208,6 +219,17 @@ function SearchBarBox(props) {
       }}
     >
       <S.SearchBarBox>
+        {componentName === "LineSet" && (
+          <S.ComboBox
+            disablePortal
+            id="combo-box-demo"
+            size="small"
+            options={top100Films}
+            renderInput={(params) => (
+              <S.TextF {...params} label="test" size="small" />
+            )}
+          />
+        )}
         <InputBox datePickerSet={datePickerSet} inputSet={inputSet} />
         <ButtonBox
           componentName={componentName}
--- src/components/onlySearchSingleGrid/searchbar/SearchBarBox.styled.js
+++ src/components/onlySearchSingleGrid/searchbar/SearchBarBox.styled.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { SEARCH_BAR_HEIGHT } from "constant/Layout";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 const SearchBarBox = styled("div")`
   height: ${SEARCH_BAR_HEIGHT};
@@ -8,4 +10,10 @@ const SearchBarBox = styled("div")`
   align-items: center;
 `;
 
-export { SearchBarBox };
+const ComboBox = styled(Autocomplete)`
+  width: 300px;
+`;
+
+const TextF = styled(TextField)``;
+
+export { SearchBarBox, ComboBox, TextF };
